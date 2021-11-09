7cce3558ff58cf1ef454ac5e76a6ac76d2b6e045
Arthur Silva <arthurmatsilva@outlook.com>
2021-11-09 10:20:01 -0300

    feat: footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,11 @@
+import github from '../../assets/github.svg';
+import './styles.css';
+
 export function Footer() {
   return (
-    <footer>
-      <h1>I'm footer</h1>
+    <footer id='main-footer'>
+      <p>Made by <a href='https://github.com/ArthurMTS' target='_blank' rel='noreferrer'>ArthurMTS</a></p>
+      <a href='https://github.com/ArthurMTS/shopping-cart' target='_blank' rel='noreferrer'><img src={github} alt='github icon' /></a>
     </footer>
   );
 }
