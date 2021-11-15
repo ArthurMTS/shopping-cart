a7ceca319dfd6a9edb204953b3db8c11c06edc7a
Arthur Silva <arthurmatsilva@outlook.com>
2021-11-15 15:28:45 -0300

    feat: contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,13 +1,47 @@
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 
+import facebookIcon from '../../assets/facebook.svg';
+import instagramIcon from '../../assets/instagram.svg';
+import linkedinIcon from '../../assets/linkedin.svg';
+import twitterIcon from '../../assets/twitter.svg';
+import youtubeIcon from '../../assets/youtube.svg';
+
 import './styles.css';
 
 export function Contact() {
   return (
     <>
       <Header />
-      <h1>Call me maybe</h1>
+      <main id='contact'>
+        <ul>
+          <li className='social-link'>
+            <a href='https://www.youtube.com/watch?v=fC7oUOUEEi4' target='_blank' rel='noreferrer'>
+              <img src={facebookIcon} alt='Facebook icon' />
+            </a>
+          </li>
+          <li className='social-link'>
+            <a href='https://www.youtube.com/watch?v=fC7oUOUEEi4' target='_blank' rel='noreferrer'>
+              <img src={instagramIcon} alt='Instagram icon' />
+            </a>
+          </li>
+          <li className='social-link'>
+            <a href='https://www.youtube.com/watch?v=fC7oUOUEEi4' target='_blank' rel='noreferrer'>
+              <img src={twitterIcon} alt='Twitter Icon' />
+            </a>
+          </li>
+          <li className='social-link'>
+            <a href='https://www.youtube.com/watch?v=fC7oUOUEEi4' target='_blank' rel='noreferrer'>
+              <img src={linkedinIcon} alt='Linkedin Icon' />
+            </a>
+          </li>
+          <li className='social-link'>
+            <a href='https://www.youtube.com/watch?v=fC7oUOUEEi4' target='_blank' rel='noreferrer'>
+              <img src={youtubeIcon} alt='Youtube icon' />
+            </a>
+          </li>
+        </ul>
+      </main>
       <Footer />
     </>
   );
